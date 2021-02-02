a1b83908c81554a31a5100cc5ca9172ca5b4433f
Juan antonio <jacc_1297@hotmail.com>
2021-02-01 20:42:19 -0600

    update typed.js

--- js/typed.js
+++ js/typed.js
@@ -1,8 +1,6 @@
 const typed = new Typed('.typed', {
 	strings: [
-		'Frontend Developer',
-		'Diseñador UI',
-		
+		'Frontend Developer'
 	],
 
 	//stringsElement: '#cadenas-texto', // ID del elemento que contiene cadenas de texto a mostrar.
@@ -12,7 +10,7 @@ const typed = new Typed('.typed', {
 	smartBackspace: true, // Eliminar solamente las palabras que sean nuevas en una cadena de texto.
 	shuffle: false, // Alterar el orden en el que escribe las palabras.
 	backDelay: 1500, // Tiempo de espera despues de que termina de escribir una palabra.
-	loop: true, // Repetir el array de strings
+	loop: false, // Repetir el array de strings
 	loopCount: false, // Cantidad de veces a repetir el array.  false = infinite
 	showCursor: true, // Mostrar cursor palpitanto
 	cursorChar: '|', // Caracter para el cursor
